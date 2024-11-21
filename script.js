0cf9a50279a7d3cfd537e735d51fd02aa5ad0e13
Normann Blum <normannblum@gmail.com>
2024-11-21 14:37:14 +0100

    endscreen design

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+let rightQuestions = 0;
+
 let currentQuestion = 0;
+
 
 function init() {
   document.getElementById("all-questions").innerHTML = questions.length;
@@ -6,15 +9,29 @@ function init() {
   showQuestion();
 }
 
+
 function showQuestion() {
+
+  if (currentQuestion >= questions.length) {
+    document.getElementById("end-screen").style = "";
+    document.getElementById("question-body").style = "display:none";
+    
+    document.getElementById('amount-of-questions').innerHTML = questions.length;
+    document.getElementById('amount-of-right-questions').innerHTML = rightQuestions;
+    document.getElementById('header-image').src = 'img/trophy.png';
+  } else {
+
   let question = questions[currentQuestion];
 
-  document.getElementById("questiontext").innerHTML = question["question"];
+  document.getElementById("question-number").innerHTML = currentQuestion + 1;
+  document.getElementById("question-text").innerHTML = question["question"];
   document.getElementById("answer_1").innerHTML = question["answer_1"];
   document.getElementById("answer_2").innerHTML = question["answer_2"];
   document.getElementById("answer_3").innerHTML = question["answer_3"];
   document.getElementById("answer_4").innerHTML = question["answer_4"];
 }
+}
+
 
 function answer(selection) {
   let question = questions[currentQuestion];
@@ -23,6 +40,7 @@ function answer(selection) {
 
   if (selcetedQuestionNumber == question["right_answer"]) {
     document.getElementById(selection).parentNode.classList.add("bg-success");
+    rightQuestions++;
   } else {
     document.getElementById(selection).parentNode.classList.add("bg-danger");
     document
@@ -32,12 +50,14 @@ function answer(selection) {
   document.getElementById("next-button").disabled = false;
 }
 
+
 function nextQuestion() {
   currentQuestion++;
   document.getElementById("next-button").disabled = true;
   resetAnswerButtons();
   showQuestion();
 }
+
 
 function resetAnswerButtons() {
   document.getElementById("answer_1").parentNode.classList.remove("bg-danger");
